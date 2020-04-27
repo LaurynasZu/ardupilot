7265aa1256ed5577efe42dbff90103333dd12f4f
LaurynasZu <64204346+LaurynasZu@users.noreply.github.com>
2020-04-27 12:15:42 +0300

    #include uncommented the rest of code commented

--- libraries/AC_PrecLand/AC_PrecLand.cpp
+++ libraries/AC_PrecLand/AC_PrecLand.cpp
@@ -1,4 +1,4 @@
-//#include <stdio.h>
+#include <stdio.h>
 #include <AP_HAL/AP_HAL.h>
 #include <AP_Scheduler/AP_Scheduler.h>
 #include <AP_AHRS/AP_AHRS.h>
@@ -386,27 +386,28 @@ bool AC_PrecLand::retrieve_los_meas(Vector3f& target_vec_unit_body)
 bool AC_PrecLand::construct_pos_meas_using_rangefinder(float rangefinder_alt_m, bool rangefinder_alt_valid)
 {
     Vector3f target_vec_unit_body;
-    Vector2f cur_pos;
-    if (_type == 5) {
-        bool alt_valid = (rangefinder_alt_valid && rangefinder_alt_m > 0.0f);
-        //_ahrs.get_relative_position_NE_home(cur_pos);
-        //hal.console->printf("chobits2: %f %f\n", cur_pos.x, cur_pos.y);
-        if (alt_valid) {
-            float alt = MAX(rangefinder_alt_m, 0.0f);
-            if (_ahrs.get_relative_position_NE_home(cur_pos)) {
-                //::printf("chobits: %f %f\n", cur_pos.x, cur_pos.y);
-                //hal.console->printf("chobits2: %f %f\n", cur_pos.x, cur_pos.y);
-                _target_pos_rel_meas_NED.x = -cur_pos.x;
-                _target_pos_rel_meas_NED.y = -cur_pos.y;
-                _target_pos_rel_meas_NED.z = alt;
-                return true;
-            }
-            else {
-                return false;
-            }
-        }
-    }
-    else if (retrieve_los_meas(target_vec_unit_body)) {
+    //Vector2f cur_pos;
+    //if (_type == 5) {
+    //    bool alt_valid = (rangefinder_alt_valid && rangefinder_alt_m > 0.0f);
+    //    //_ahrs.get_relative_position_NE_home(cur_pos);
+    //    //hal.console->printf("chobits2: %f %f\n", cur_pos.x, cur_pos.y);
+    //    if (alt_valid) {
+    //        float alt = MAX(rangefinder_alt_m, 0.0f);
+    //        if (_ahrs.get_relative_position_NE_home(cur_pos)) {
+    //            //::printf("chobits: %f %f\n", cur_pos.x, cur_pos.y);
+    //            //hal.console->printf("chobits2: %f %f\n", cur_pos.x, cur_pos.y);
+    //            _target_pos_rel_meas_NED.x = -cur_pos.x;
+    //            _target_pos_rel_meas_NED.y = -cur_pos.y;
+    //            _target_pos_rel_meas_NED.z = alt;
+    //            return true;
+    //        }
+    //        else {
+    //            return false;
+    //        }
+    //    }
+    //}
+    //else if (retrieve_los_meas(target_vec_unit_body)) {
+    if (retrieve_los_meas(target_vec_unit_body)) {
         const struct inertial_data_frame_s *inertial_data_delayed = (*_inertial_history)[0];
 
         Vector3f target_vec_unit_ned = inertial_data_delayed->Tbn * target_vec_unit_body;
